6decada1e5fc15923b90728f9fd8b19a3344da53
Julio C. Prudencio <pjulio1234@gmail.com>
2024-08-29 10:42:03 -0700

    Initial commit for my portfolio webpage

---
+++ app.py
@@ -0,0 +1,105 @@
+from flask import Flask, redirect, url_for, jsonify , request, render_template
+
+import logging
+# create logger
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.DEBUG)
+# create console handler and set level to debug
+ch = logging.StreamHandler()
+ch.setLevel(logging.DEBUG) # can change from debugg to something else like .info which will give us info
+formatter = logging.Formatter('%(asctime)s - %(name)s %(funcName)s():%(lineno)i: - %(levelname)s - %(message)s')
+ch.setFormatter(formatter)
+logger.addHandler(ch)
+
+import boto3
+from botocore.exceptions import ClientError
+
+
+app = Flask(__name__) 
+
+
+
+@app.route('/del_name')
+def del_name():
+
+    #client = boto3.client('s3')
+    #client.delete_object(Bucket='fav-name-storage' , Key= 'fav-name.txt')
+    clientdb = boto3.client('dynamodb')
+    
+    response = clientdb.delete_item(
+        TableName='tblname2',
+        Key={
+            'userId': {'S': request.args.get('name')} 
+            }
+            )
+    return jsonify(status="ok" , name= request.args.get('name') )
+
+
+
+
+@app.route('/get_cred')
+def get_cred():
+    clientdb = boto3.client('dynamodb')
+    try:
+        response = clientdb.get_item(
+            TableName='tblname2',
+            Key={
+                'userId': {'S' : request.args.get('name')}
+            }
+            )
+        db_psw = response['Item']['psw']['S'] # password in database
+        input_psw = request.args.get('password') # this is th password from the html page
+        if db_psw == input_psw:
+            return jsonify(sucess=True)         
+    except: 
+        return jsonify(sucess=False , status="Invalid password")
+
+    #return jsonify(status="ok", request_name = request.args.get('name'), 
+    #db_name = response['Item']['userId']['S'], db_password = response['Item']['psw']['S']
+    #,password = request.args.get('password'))
+
+#    client= boto3.client('s3')
+#    try:
+#        obj=client.get_object(Bucket='fav-food-storage' , Key='fav_food.txt')
+#    except: 
+#        return jsonify(status='error')
+
+
+
+
+
+@app.route('/register_user')
+def register_user(): 
+    clientdb = boto3.client('dynamodb')
+    clientdb.put_item(
+        TableName= 'tblname2', 
+        Item={
+            'userId': {'S' : request.args.get('name')}, 
+            'psw' : {'S' : request.args.get('password')}
+            }
+            )
+#    client = boto3.client('s3')
+#    client.put_object(
+#            Body = request.args.get('name'), 
+#            Bucket='fav-name-storage',
+#            Key='fav-name.txt' ,  
+#            ContentType='text/plain')
+
+    return jsonify(status="ok", name=request.args.get('name') , password=request.args.get('password'))
+
+
+@app.route('/')
+def redir_to_static_home():
+    return redirect('/static/index.html')
+
+
+if __name__ == '__main__': 
+
+    logger.debug("logger is fucntional")
+    app.run(host='0.0.0.0')
+
+
+
+
+
+
