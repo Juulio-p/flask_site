7d0a9a45c0d05f0347c15530b625a29b8c9bafee
Julio C. Prudencio <pjulio1234@gmail.com>
2026-02-06 01:19:54 -0800

    Fixed routes via aws, but deleted the contact us i put in last time

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -27,7 +27,6 @@ function App() {
                 <Frontpage />
                 <TierCard />
                 <Footer />
-                <ContactUs />
               </>
             }
           />
